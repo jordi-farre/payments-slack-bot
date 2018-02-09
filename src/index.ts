5a6e34c69c9793ba5c6ef14d8baf7347c02a69a9
Jordi Farré <jordifr@gmail.com>
2018-02-09 10:44:51 +0100

    implemented json post service

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,10 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.use(bodyParser.json({ strict: false }));
 
-app.get('/', function (req, res) {
-  console.log(req);
+app.post('/', function (req, res) {
+  console.log(req.body);
   res.json({message : "test"})
 });
 
