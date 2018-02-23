015b84108fe9f9e6df88eb370627d276063ecd18
Jordi Farré <jordifr@gmail.com>
2018-02-23 21:04:41 +0100

    split controller in little methods to better understanding

--- src/BotController.ts
+++ src/BotController.ts
@@ -10,28 +10,57 @@ export class BotController {
   }
 
   process(request, response) {
-    if (this.botToken == request.body.token) {
-      if (request.body.type == "url_verification") {
-          response.json({"challenge": request.body.challenge});
-      } else if (request.body.type == "event_callback" && request.body.event.type == "app_mention") {
-          const echoRegexp = /echo (.*)/i;
-          const match = echoRegexp.exec(request.body.event.text);
-	  if (match) {
-	    this.webClient.chat.postMessage(request.body.event.channel, match[1], (err, res) => {
-	      if (err) {
-	        console.error(err);
-                response.status(500).send(err);
-	      } else {
-		response.status(200).send("OK");
-	      }
-	    });
-	  }
+    if (this.isValidTokenFor(request)) {
+      if (this.isChallenge(request)) {
+        this.handleChallenge(request, response);
+      } else if (this.isAppMention(request)) {
+        this.handleAppMention(request, response);
       } else {
-          response.status(200).send("OK");
+        this.handleUnknown(request, response);
       }
     } else {
-      response.status(401).send("incorrect bot token");
+      this.handleUnauthorized(request, response);
     }
   }
+
+  isValidTokenFor(request) {
+    return this.botToken == request.body.token;
+  }
+
+  isChallenge(request) {
+    return request.body.type == "url_verification";
+  }
+  
+  handleChallenge(request, response) {
+    response.json({"challenge": request.body.challenge});
+  }
+
+  isAppMention(request) {
+    return request.body.type == "event_callback" && request.body.event.type == "app_mention";
+  }
+
+  handleAppMention(request, response) {
+    const echoRegexp = /echo (.*)/i;
+    const match = echoRegexp.exec(request.body.event.text);
+    if (match) {
+      this.webClient.chat.postMessage(request.body.event.channel, match[1], (err, res) => {
+        if (err) {
+          console.error(err);
+          response.status(500).send(err);
+        } else {
+          response.status(200).send("OK");
+        }
+      });
+    }
+  }
+
+  handleUnknown(request, response) {
+    response.status(200).send("OK");
+  }
+
+  handleUnauthorized(request, response) {
+    response.status(401).send("incorrect bot token");
+  }
+
 
 }
